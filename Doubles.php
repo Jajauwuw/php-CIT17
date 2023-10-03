9a7a306b087fe13fee94217c7180db9cd4ace310
M303user <m303user@M303.uc-bcf.edu.ph>
2023-10-03 17:52:38 +0800

    add php

---
+++ Doubles.php
@@ -0,0 +1,9 @@
+<?php
+  // Doubles
+many = 2.2888800;
+$many_2 = 2.2111200;
+$few = $many + $many_2;
+print(.$many + $many_2 = $few<br>.);
+
+
+?>
---
+++ Variables.php
@@ -0,0 +1,9 @@
+<?php
+  // Integers
+  int_var = 12345;
+$another_int = -12345 + 12345;
+
+
+
+
+?>
